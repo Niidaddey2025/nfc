771242c23c63531878522ac64e0e2057527f02ae
Niidaddey2025 <sdardey1221@gmail.com>
2025-05-13 16:06:05 +0000

    new update

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const cardRouter = require('./routes/card.route');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(cors());
+app.use(cors("*"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
